8a8e2a31c2bb581cf455e8e44179faa54141aa2c
sandovaldavid <sandovaldavid2201@gmail.com>
2024-08-03 01:53:28 -0500

    Check fonts

--- app/ui/fonts.tsx
+++ app/ui/fonts.tsx
@@ -1,2 +1,7 @@
-import {Montserrat} from 'next/font/google';
-const montserrat = Montserrat({subsets: ['latin']});
+import { Montserrat, Lusitana } from "next/font/google";
+
+export const montserrat = Montserrat({ subsets: ["latin"] });
+export const lusitana = Lusitana({
+	subsets: ["latin"],
+	weight: ["400", "700"],
+});
